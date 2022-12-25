66ce820370e353c177d5d8bb61cf882355f2b082
whoisarjen <Kamilow97@gmail.com>
2022-12-25 22:52:13 +0100

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -4,7 +4,7 @@ const withPWA = require("next-pwa")({
     dest: 'public'
 })
 
-const isDev = process.env.NODE_ENV === 'development'
+const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
     reactStrictMode: true,
@@ -12,14 +12,14 @@ module.exports = withPWA({
         dest: "public",
         register: true,
         skipWaiting: true,
-        disable: isDev,
+        disable: !isProduction,
     },
     env: {
         PRODUCT_DECIMAL_PLACES: 1,
         DEFAULT_NUMBER_OF_MEALS: 5,
         MAX_SUPPORTED_AGE_IN_DAYS: 7,
         SEARCH_MIN_NAME_LENGTH: 3,
-        SERVER: isDev ? 'http://localhost:8000/graphql' : 'https://server.juicify.app/graphql',
+        SERVER: isProduction ? 'https://server.juicify.app/graphql' : 'http://localhost:8000/graphql',
         INDEXEDDB: 'cache',
         APP_VERSION: new Date().toISOString(),
     },
