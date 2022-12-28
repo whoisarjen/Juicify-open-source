39b0320b94c2f659b9dee96618b4ee8bd6ffc301
whoisarjen <Kamilow97@gmail.com>
2022-12-28 12:55:01 +0100

    not supported browsers moved to default next config

--- client/layout/Layout.tsx
+++ client/layout/Layout.tsx
@@ -72,62 +72,6 @@ const getCookie = async (cookieName: string) => {
     return cookie[cookieName];
 }
 
-const isBrowserValid = async () => {
-    let agent: any = { browser: { name: null, version: null, v: null, userAgent: null, app: null } };
-    let nAgt: any = navigator.userAgent;
-    let browserName: any = navigator.appName;
-    let fullVersion: any = '' + parseFloat(navigator.appVersion);
-    let majorVersion: any = parseInt(navigator.appVersion, 10);
-    let nameOffset, verOffset;
-    if ((verOffset = nAgt.indexOf("Opera")) != -1) {
-        browserName = "Opera";
-        fullVersion = nAgt.substring(verOffset + 6);
-        if ((verOffset = nAgt.indexOf("Version")) != -1)
-            fullVersion = nAgt.substring(verOffset + 8);
-    }
-    else if ((verOffset = nAgt.indexOf("MSIE")) != -1) {
-        browserName = "Microsoft Internet Explorer";
-        fullVersion = nAgt.substring(verOffset + 5);
-    }
-    else if ((verOffset = nAgt.indexOf("Chrome")) != -1) {
-        browserName = "Chrome";
-        fullVersion = nAgt.substring(verOffset + 7);
-    }
-    else if ((verOffset = nAgt.indexOf("Safari")) != -1) {
-        browserName = "Safari";
-        fullVersion = nAgt.substring(verOffset + 7);
-        if ((verOffset = nAgt.indexOf("Version")) != -1)
-            fullVersion = nAgt.substring(verOffset + 8);
-    }
-    else if ((verOffset = nAgt.indexOf("Firefox")) != -1) {
-        browserName = "Firefox";
-        fullVersion = nAgt.substring(verOffset + 8);
-    }
-    else if ((nameOffset = nAgt.lastIndexOf(' ') + 1) < (verOffset = nAgt.lastIndexOf('/'))) {
-        browserName = nAgt.substring(nameOffset, verOffset);
-        fullVersion = nAgt.substring(verOffset + 1);
-        if (browserName.toLowerCase() == browserName.toUpperCase()) {
-            browserName = navigator.appName;
-        }
-    }
-    majorVersion = parseInt('' + fullVersion, 10);
-    if (isNaN(majorVersion)) {
-        fullVersion = '' + parseFloat(navigator.appVersion);
-        majorVersion = parseInt(navigator.appVersion, 10);
-    }
-    agent.browser.name = browserName;
-    agent.browser.version = fullVersion;
-    agent.browser.v = majorVersion;
-    agent.browser.app = navigator.appName;
-    agent.browser.userAgent = navigator.userAgent;
-    if (agent.browser.name == "Microsoft Internet Explorer" && parseInt(agent.browser.v) < 100) return false // Not supporting IE
-    if (agent.browser.name == "Chrome" && parseInt(agent.browser.v) < 4) return false
-    if (agent.browser.name == "Firefox" && parseInt(agent.browser.v) < 11) return false
-    if (agent.browser.name == "Opera" && parseInt(agent.browser.v) < 13) return false
-    if (agent.browser.name == "Safari" && parseInt(agent.browser.v) < 5) return false
-    return true;
-}
-
 const Layout = ({ children }: { children: any }) => {
     const router = useRouter()
     const [isAllowedLocation, setIsAllowedLocation] = useState(false)
@@ -135,10 +79,6 @@ const Layout = ({ children }: { children: any }) => {
 
     useEffect(() => {
         (async () => {
-            if (!(await isBrowserValid()) && router.pathname != '/not-supported') {
-                return router.push('/not-supported')
-            }
-
             const locale = await getCookie('NEXT_LOCALE') // Redirect for PWA's scope
             if (locale && router.locale != locale) {
                 return router.push(router.asPath, router.asPath, { locale });
