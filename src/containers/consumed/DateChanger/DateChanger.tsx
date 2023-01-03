ad6bcdb040c1361c60df6336e4e4ad88708e1bde
whoisarjen <Kamilow97@gmail.com>
2023-01-03 10:42:50 +0100

    fix DateChanger

--- src/containers/consumed/DateChanger/DateChanger.tsx
+++ src/containers/consumed/DateChanger/DateChanger.tsx
@@ -1,25 +1,26 @@
-import Dialog from '@mui/material/Dialog';
-import Button from '@mui/material/Button';
-import { useRouter } from "next/router";
-import EventIcon from '@mui/icons-material/Event';
-import IconButton from '@mui/material/IconButton';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import { useState } from "react";
-import CalendarPicker from '@mui/lab/CalendarPicker';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import SlideUp from '@/transition/SlideUp';
+import Dialog from '@mui/material/Dialog'
+import Button from '@mui/material/Button'
+import { useRouter } from "next/router"
+import EventIcon from '@mui/icons-material/Event'
+import IconButton from '@mui/material/IconButton'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import { useState } from "react"
+import { CalendarPicker } from '@mui/x-date-pickers'
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import SlideUp from '@/transition/SlideUp'
+import moment from 'moment'
 
 const DateChanger = ({ where = 'consumed' }: { where?: string }) => {
     const router = useRouter()
     const [isDialog, setIsDialog] = useState(false)
-    const [value, setValue] = useState<Date>(new Date());
+    const [value, setValue] = useState(moment().toDate())
 
     const handleDateChange = () => {
-        setIsDialog(false);
-        router.push(`/${router.query.login}/${where}/${new Date(value).toJSON().slice(0, 10)}`)
-    };
+        setIsDialog(false)
+        router.push(`/${router.query.login}/${where}/${moment(value).format('YYYY-MM-DD')}`)
+    }
 
     return (
         <>
@@ -33,8 +34,11 @@ const DateChanger = ({ where = 'consumed' }: { where?: string }) => {
                 aria-describedby="alert-dialog-slide-description"
             >
                 <DialogContent>
-                    <LocalizationProvider dateAdapter={AdapterDateFns}>
-                        <CalendarPicker date={value} onChange={(newDate: any) => setValue(newDate || new Date())} />
+                    <LocalizationProvider dateAdapter={AdapterMoment}>
+                        <CalendarPicker
+                            date={moment(value)}
+                            onChange={newDate => setValue(moment(newDate || moment()).toDate())}
+                        />
                     </LocalizationProvider>
                 </DialogContent>
                 <DialogActions>
