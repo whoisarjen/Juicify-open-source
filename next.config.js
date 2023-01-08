2fc6b9f2b50edb13cfb4b3332f93ead4a39d7a46
whoisarjen <Kamilow97@gmail.com>
2023-01-08 22:17:02 +0100

    docker compose for production

--- next.config.js
+++ next.config.js
@@ -16,8 +16,12 @@ const nextConfig = {
     env: {
         NEXT_PUBLIC_SEARCH_MIN_NAME_LENGTH: 3,
         NEXT_PUBLIC_DEFAULT_NUMBER_OF_MEALS: 5,
-        SERVER: isProduction ? 'https://server.juicify.app/graphql' : 'http://localhost:8000/graphql',
         APP_VERSION: new Date().toISOString(),
+        NEXTAUTH_SECRET: "w@yYo%aJ;]#FXj{;j?@d`#48NHrC'eT]5DyLQD@Bv0LWkivPE)~X@sGB,v;ev/)",
+        DISCORD_CLIENT_ID: "1056593142381547630",
+        DISCORD_CLIENT_SECRET: "xO5xLnwaxH9y2e3O4egNNWfp4R0273fN",
+        GOOGLE_CLIENT_ID: "658821628417-vpspbk50qh0qb7cbg6d4h6hd2e9up3h2.apps.googleusercontent.com",
+        GOOGLE_CLIENT_SECRET: "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",
         isProduction,
     },
     ...nextTranslate(),
