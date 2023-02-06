24cca8662b67b4a56517248b024793ac49446a16
Kamil Owczarek <Kamilow97@gmail.com>
2023-02-06 01:48:48 +0100

    renaming posts

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -30,20 +30,26 @@ const Content = styled.div`
     max-width: 870px;
 `
 
-const PostPage = () => {
-    const { title, img_url, content } = {
-        title: 'The Witcher: Ronin has arrived on Kickstarter!',
-        content: `The Witcher: Ronin is an original comic, created and published by CD PROJEKT RED, which translates the dark fantasy world of The Witcher into a feudal Japanese setting. The story focuses on monster slayer Geralt, who must venture across Yokai - infested lands while attempting to track down the mysterious Lady of Snow Yuki Onna.`,
-        img_url: '/images/witcher.jpg'
-    }
+const { title, img_url, content } = {
+    title: 'The Witcher: Ronin has arrived on Kickstarter!',
+    content: `The Witcher: Ronin is an original comic, created and published by CD PROJEKT RED, which translates the dark fantasy world of The Witcher into a feudal Japanese setting. The story focuses on monster slayer Geralt, who must venture across Yokai - infested lands while attempting to track down the mysterious Lady of Snow Yuki Onna.`,
+    img_url: '/images/witcher.jpg'
+}
 
+const PostPage = () => {
     return (
         <Box>
             <Image src={img_url} alt="Juicify" width="970" height="545" />
             <ContentBox>
                 <Content>
                     <h1>{title}</h1>
-                    {range(10).map(x => <p key={x}>{content}</p>)}
+                    {range(5).map(x => <p key={x}>{content}</p>)}
+                    <h2>asddassadasasas</h2>
+                    {range(5).map(x => <p key={x}>{content}</p>)}
+                    <h3>zxcxzcxzczxzcx</h3>
+                    {range(5).map(x => <p key={x}>{content}</p>)}
+                    <h2>asddassadasasas</h2>
+                    {range(5).map(x => <p key={x}>{content}</p>)}
                 </Content>
                 <SidebarRight />
             </ContentBox>
