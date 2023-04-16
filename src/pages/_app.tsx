320ef964b75f6d5e42354983d6daf902e24dfbc8
Kamil Owczarek <Kamilow97@gmail.com>
2023-04-16 17:26:48 +0200

    GoogleAnalytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
-import "../styles/global.css"
-import Layout from "../layout/Layout"
-import MUI from "../layout/MUI"
-import Header from "../layout/Header"
+import '../styles/global.css'
+import Layout from '../layout/Layout'
+import MUI from '../layout/MUI'
+import Header from '../layout/Header'
 import { trpc } from '@/utils/trpc.utils'
-import { SessionProvider } from "next-auth/react"
+import { SessionProvider } from 'next-auth/react'
 import { type AppType } from 'next/app'
-import { type Session } from "next-auth"
+import { type Session } from 'next-auth'
+import { GoogleAnalytics } from 'src/layout/GoogleAnalytics'
 
 const App: AppType<{ session: Session | null }> = ({
     Component,
@@ -14,6 +15,7 @@ const App: AppType<{ session: Session | null }> = ({
     return (
         <MUI>
             <Header />
+            <GoogleAnalytics />
             <SessionProvider session={session}>
                 <Layout>
                     <Component {...pageProps} />
