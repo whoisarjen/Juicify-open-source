2f468adc15b730ed3a2ccf5e0bbadeda26adb484
Kamil Owczarek <Kamilow97@gmail.com>
2023-06-26 17:42:47 +0200

    v

--- init-scripts/create_juicify.sql
+++
@@ -1,6 +0,0 @@
-DO $$BEGIN
-IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'juicify') THEN
-    CREATE DATABASE juicify;
-    GRANT ALL PRIVILEGES ON DATABASE juicify TO admin;
-END IF;
-END$$;
--- init-scripts/create_juicify_strapi.sql
+++
@@ -1,6 +0,0 @@
-DO $$BEGIN
-IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'juicify_strapi') THEN
-    CREATE DATABASE juicify_strapi;
-    GRANT ALL PRIVILEGES ON DATABASE juicify_strapi TO admin;
-END IF;
-END$$;
