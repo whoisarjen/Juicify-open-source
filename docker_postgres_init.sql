322a75815821cabb0814c28310f4d771b6549bd8
Kamil Owczarek <Kamilow97@gmail.com>
2023-06-26 16:06:43 +0200

    v

---
+++ docker_postgres_init.sql
@@ -0,0 +1,18 @@
+CREATE USER admin WITH PASSWORD 'admin' CREATEDB;
+CREATE DATABASE juicify
+    WITH 
+    OWNER = admin
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+CREATE DATABASE juicify_strapi
+    WITH 
+    OWNER = admin
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
