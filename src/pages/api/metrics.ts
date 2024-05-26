be241b495f48a8e1012b4b4b704e94c3a48a2f0c
Kamil Owczarek <80225782+whoisarjen@users.noreply.github.com>
2024-05-26 23:10:17 +0000

    Turn OFF prometheus

--- src/pages/api/metrics.ts
+++ src/pages/api/metrics.ts
@@ -1,9 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { register, collectDefaultMetrics } from "prom-client";
+// import { register, collectDefaultMetrics } from "prom-client";
 
-collectDefaultMetrics({ prefix: 'juicify' })
+// collectDefaultMetrics({ prefix: 'juicify' })
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    res.setHeader('Content-type', register.contentType)
-    res.end(await register.metrics())
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//     res.setHeader('Content-type', register.contentType)
+//     res.end(await register.metrics())
+// }
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+  ) {
+    res.status(500).send({ error: 'Metrics turned OFF!' })
 }
--- src/server/trpc/trpc.ts
+++ src/server/trpc/trpc.ts
@@ -4,21 +4,21 @@ import superjson from "superjson";
 import { getClientIp } from 'request-ip'
 import { type Context } from "./context";
 // import { SPAM_PROTECTION_LIMIT_FOR_CALLS } from '@/utils/trpc.utils'
-import { Histogram, register } from "prom-client"
+// import { Histogram, register } from "prom-client"
 
-register.clear();
+// register.clear();
 
-const restResponseTimeHistogram = new Histogram({
-    name: 'rest_response_time_duration_milliseconds',
-    help: 'Rest response time of API calls in milliseconds',
-    labelNames: ['ip', 'path', 'type', 'status'],
-})
+// const restResponseTimeHistogram = new Histogram({
+//     name: 'rest_response_time_duration_milliseconds',
+//     help: 'Rest response time of API calls in milliseconds',
+//     labelNames: ['ip', 'path', 'type', 'status'],
+// })
 
-const dbResponseTimeHistogram = new Histogram({
-    name: 'db_response_time_duration_milliseconds',
-    help: 'DB response time of API calls in milliseconds',
-    labelNames: ['ip', 'path', 'type', 'status'],
-})
+// const dbResponseTimeHistogram = new Histogram({
+//     name: 'db_response_time_duration_milliseconds',
+//     help: 'DB response time of API calls in milliseconds',
+//     labelNames: ['ip', 'path', 'type', 'status'],
+// })
 
 const t = initTRPC.context<Context>().create({
     transformer: superjson,
@@ -41,74 +41,74 @@ const isAuthed = t.middleware(({ ctx, next }) => {
     });
 });
 
-const spamProtectionMiddleware = t.middleware(async ({ ctx: { req }, next }) => {
-    // const userIp = getClientIp(req)
+// const spamProtectionMiddleware = t.middleware(async ({ ctx: { req }, next }) => {
+//     const userIp = getClientIp(req)
 
-    // if (userIp) {
-    //     const currentStatusOfIp = await redis.get(userIp)
-    //     const updatedStatusOfIp = Number(currentStatusOfIp || 0) + 1
+//     if (userIp) {
+//         const currentStatusOfIp = await redis.get(userIp)
+//         const updatedStatusOfIp = Number(currentStatusOfIp || 0) + 1
 
-    //     if (updatedStatusOfIp >= SPAM_PROTECTION_LIMIT_FOR_CALLS.NUMBER_OF_CALLS) {
-    //         await redis.set(
-    //             userIp,
-    //             SPAM_PROTECTION_LIMIT_FOR_CALLS.NUMBER_OF_CALLS,
-    //             { EX: SPAM_PROTECTION_LIMIT_FOR_CALLS.BAN_DURATION },
-    //         )
+//         if (updatedStatusOfIp >= SPAM_PROTECTION_LIMIT_FOR_CALLS.NUMBER_OF_CALLS) {
+//             await redis.set(
+//                 userIp,
+//                 SPAM_PROTECTION_LIMIT_FOR_CALLS.NUMBER_OF_CALLS,
+//                 { EX: SPAM_PROTECTION_LIMIT_FOR_CALLS.BAN_DURATION },
+//             )
 
-    //         throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
-    //     }
+//             throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+//         }
 
-    //     await redis.set(
-    //         userIp,
-    //         updatedStatusOfIp,
-    //         { EX: SPAM_PROTECTION_LIMIT_FOR_CALLS.DURATION },
-    //     )
-    // }
+//         await redis.set(
+//             userIp,
+//             updatedStatusOfIp,
+//             { EX: SPAM_PROTECTION_LIMIT_FOR_CALLS.DURATION },
+//         )
+//     }
 
-    return await next();
-});
+//     return await next();
+// });
 
-const prometheusRestResponseMiddleware = t.middleware(async ({ path, type, ctx: { req }, next }) => {
-    const ip = getClientIp(req) || ''
-    const start = Date.now();
-    const result = await next();
-    const durationMs = Date.now() - start;
+// const prometheusRestResponseMiddleware = t.middleware(async ({ path, type, ctx: { req }, next }) => {
+//     const ip = getClientIp(req) || ''
+//     const start = Date.now();
+//     const result = await next();
+//     const durationMs = Date.now() - start;
 
-    restResponseTimeHistogram.observe({
-        ip,
-        path,
-        type,
-        status: result.ok ? 'OK' : 'Non-OK',
-    }, durationMs)
+//     restResponseTimeHistogram.observe({
+//         ip,
+//         path,
+//         type,
+//         status: result.ok ? 'OK' : 'Non-OK',
+//     }, durationMs)
 
-    return result;
-});
+//     return result;
+// });
 
-const prometheusDBResponseMiddleware = t.middleware(async ({ path, type, ctx: { req }, next }) => {
-    const ip = getClientIp(req) || ''
-    const start = Date.now();
-    const result = await next();
-    const durationMs = Date.now() - start;
+// const prometheusDBResponseMiddleware = t.middleware(async ({ path, type, ctx: { req }, next }) => {
+//     const ip = getClientIp(req) || ''
+//     const start = Date.now();
+//     const result = await next();
+//     const durationMs = Date.now() - start;
 
-    dbResponseTimeHistogram.observe({
-        ip,
-        path,
-        type,
-        status: result.ok ? 'OK' : 'Non-OK',
-    }, durationMs)
+//     dbResponseTimeHistogram.observe({
+//         ip,
+//         path,
+//         type,
+//         status: result.ok ? 'OK' : 'Non-OK',
+//     }, durationMs)
 
-    return result;
-});
+//     return result;
+// });
 
 export const publicProcedure = t
     .procedure
-    .use(prometheusRestResponseMiddleware)
+    // .use(prometheusRestResponseMiddleware)
     // .use(spamProtectionMiddleware)
-    .use(prometheusDBResponseMiddleware)
+    // .use(prometheusDBResponseMiddleware)
 
 export const protectedProcedure = t
     .procedure
-    .use(prometheusRestResponseMiddleware)
+    // .use(prometheusRestResponseMiddleware)
     // .use(spamProtectionMiddleware)
-    .use(prometheusDBResponseMiddleware)
+    // .use(prometheusDBResponseMiddleware)
     .use(isAuthed)
