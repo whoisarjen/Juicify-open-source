e3857310bfc01f3d9b638ce41052c01af4cfed69
Kamil Owczarek <80225782+whoisarjen@users.noreply.github.com>
2024-05-26 23:03:30 +0000

    turn off fs

--- src/server/trpc/router/version.ts
+++ src/server/trpc/router/version.ts
@@ -1,13 +1,14 @@
 import { router, publicProcedure } from "../trpc";
-import fs from 'fs'
-import path from 'path'
+// import fs from 'fs'
+// import path from 'path'
 
 export const versionRouter = router({
     get: publicProcedure
         .query(() => {
-            const dir = path.resolve('./public', '')
-            const filenames = fs.readdirSync(dir)
+            return ''
+            // const dir = path.resolve('./public', '')
+            // const filenames = fs.readdirSync(dir)
 
-            return filenames.find(filename => filename.includes('fallback')) || ''
+            // return filenames.find(filename => filename.includes('fallback')) || ''
         }),
 });
