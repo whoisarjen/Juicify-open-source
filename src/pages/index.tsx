582136f3da999e6e1985d54ae46a08e66915900e
„Kamil” <Kamilow97@gmail.com>
2025-08-28 19:31:31 +0200

    Revert "Add comprehensive landing page for non-authenticated users"
    This reverts commit edc0c66500dbb8b7ead710a4f8ad325e2eed612b.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,14 @@ import {
     type ClientSafeProvider,
     getProviders,
     LiteralUnion,
+    signIn,
     useSession,
 } from 'next-auth/react'
-import LandingPage from '@/components/LandingPage'
+import Logo from '@/components/Logo/Logo'
 import { useEffect, useState } from 'react'
 import { type BuiltInProviderType } from 'next-auth/providers'
+import Button from '@mui/material/Button'
+import Stack from '@mui/material/Stack'
 
 const Home = () => {
     const { data: sessionData } = useSession()
@@ -27,7 +30,28 @@ const Home = () => {
         }
     }, [sessionData])
 
-    return <LandingPage providers={providers} />
+    return (
+        <div className="flex flex-col items-center justify-around flex-1">
+            <Logo size={175} />
+            {/* TODO ACCEPT RULES + TRANSLATE WHOLE SITE (NEED TO CHECK WHERE) */}
+            <Stack spacing={2} direction="column">
+                {providers &&
+                    Object.values(providers).map((provider) => (
+                        <Button
+                            variant="outlined"
+                            key={provider.name}
+                            onClick={() =>
+                                signIn(provider.id, {
+                                    callbackUrl: 'http://juicify.app/coach',
+                                })
+                            }
+                        >
+                            Sign in with {provider.name}
+                        </Button>
+                    ))}
+            </Stack>
+        </div>
+    )
 }
 
 export default Home
