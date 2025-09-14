b7b4b365bea08808e84e00e9f4caf9dbf263be88
„Kamil” <Kamilow97@gmail.com>
2025-09-14 21:38:47 +0200

    Revert "predefined"
    This reverts commit 81307ee1ae91b4af9d369fd2902770da7c4d4728.

--- src/pages/[login]/workout/plans/index.tsx
+++ src/pages/[login]/workout/plans/index.tsx
@@ -34,18 +34,8 @@ const WorkoutPlansPage = () => {
         },
     })
 
-    const addPredefinedPlan = trpc.workoutPlan.addPredefinedPlan.useMutation({
-        onSuccess: () => {
-            utils.workoutPlan.getAll.invalidate({ username })
-        },
-    })
-
     const handleCreateWorkoutPlan = async () => {
         await createWorkoutPlan.mutateAsync({ name: 'ABC' })
-    }
-
-    const handleAddPredefinedPlan = async () => {
-        await addPredefinedPlan.mutateAsync()
     }
 
     const isOwner = router.query.login == sessionData?.user?.username
@@ -53,18 +43,7 @@ const WorkoutPlansPage = () => {
     return (
         <div className="flex flex-1 flex-col gap-4">
             {isOwner && <NavbarOnlyTitle title="workout:WORKOUT_PLANS" />}
-            {isOwner && (
-                <div className="flex gap-2">
-                    <ButtonPlusIcon onClick={handleCreateWorkoutPlan} />
-                    <button
-                        onClick={handleAddPredefinedPlan}
-                        disabled={addPredefinedPlan.isLoading}
-                        className="flex-1 rounded-lg bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 disabled:opacity-50"
-                    >
-                        {addPredefinedPlan.isLoading ? 'Adding...' : 'Add Sample Workout Plan'}
-                    </button>
-                </div>
-            )}
+            {isOwner && <ButtonPlusIcon onClick={handleCreateWorkoutPlan} />}
             {!isOwner && <NavbarProfile tab={3} />}
             <BoxWorkoutLoader isLoading={isFetching}>
                 <>
