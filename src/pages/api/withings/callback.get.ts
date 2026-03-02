b319b33aa88578c79e9c6bb1b7229b0ff6b0c05b
Kamil Owczarek <Kamilow97@gmail.com>
2026-03-02 18:37:39 +0800

    v

--- src/pages/api/withings/callback.get.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    res.send({
-        code: 200,
-        message: 'Withings callback received successfully',
-    })
-}
